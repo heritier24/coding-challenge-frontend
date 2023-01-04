86bae0910e48e9945766a1ee78ff6096ffbb8d91
heritier24 <heritier.ganza@kudibooks.com>
2023-01-04 02:19:12 +0200

    installed data table package, axios  and use it in the app

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import DataTable from '@andresouzaabreu/vue-data-table'
+import '@andresouzaabreu/vue-data-table/dist/DataTable.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+Vue.component('data-table', DataTable)
 
 Vue.config.productionTip = false
 
